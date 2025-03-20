d7f0f65d50a9eea215d58c32870fc9ba2774c5a6
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-20 17:16:01 +0300

    upload user profile

--- src/app/(pages)/dashboard/profile/page.tsx
+++ src/app/(pages)/dashboard/profile/page.tsx
@@ -2,11 +2,6 @@
 import Image from "next/image";
 import {useEffect, useState} from "react";
 
-type User ={
-    name:string,
-    id:number,
-    userEmail:string
-}
 export default function ProfilePage(){
     const[username,setUsername]=useState<string>("")
     const[email,setEmail]=useState<string>("")
@@ -15,9 +10,12 @@ export default function ProfilePage(){
     const[mobile,setMobile]=useState<string>("")
     const[description,setDescription]=useState<string>("")
     const[message,setMessage]=useState<string>("")
+    const[uploadedProfileImage,setUploadedProfileImage] =useState<Blob|undefined>(undefined)
     const userId =1
     const userUrl =`http://localhost:8080/api/v1/users/${userId}/get`
-
+    const uploadUrl =`http://localhost:8080/api/v1/users/${userId}/upload-profile-photo`
+    const[showChangeProfileImageForm,setShowProfileImageForm] =useState(false)
+    const [userProfileImage,setUserProfileImage]=useState<string>("https://i.pinimg.com/236x/be/38/78/be3878c34f93d1663a6e5f6af4b78e9c.jpg")
     useEffect(() => {
         getUserProfile()
     }, []);
@@ -34,8 +32,15 @@ export default function ProfilePage(){
                     setMessage(data.message)
                     return
                 }
-                setUserInfo(data?.user)
-
+                const byteCharacters =atob(data?.user?.profileImage)
+                const byteNumbers = new Array(byteCharacters.length);
+                for (let i = 0; i < byteCharacters.length; i++) {
+                    byteNumbers[i] = byteCharacters.charCodeAt(i);
+                }
+                const byteArray = new Uint8Array(byteNumbers);
+                const blob = new Blob([byteArray]);
+                const myUrl =URL.createObjectURL(blob)
+                setUserProfileImage(myUrl)
                 // setUsername(data?.user?.username)
                 // setLinkedIn(data.user?.linkedin !==null?data.user?.linkedin:"")
                 // setGithub(data.user?.githubAccount !==null?data.user?.githubAccount:"add")
@@ -47,14 +52,40 @@ export default function ProfilePage(){
             setMessage(">"+e)
         }
     })
+    const uploadProfilePhoto =(async ()=>{
+        const formData =new FormData()
+        if(typeof (uploadedProfileImage) !="undefined"){
+            formData.append("image",uploadedProfileImage)
+        }else{
+            setMessage("Profile photo is undefined")
+            return
+        }
+        try{
+            const response =await fetch(uploadUrl,{
+                method:"PUT",
+                body:formData
+            })
+            if(response.ok){
+                const data =await response.json()
+                if(data.statusCode !=200){
+                    setMessage(data.message)
+                }else{
+                    setMessage(data.message)
+                }
+            }
+        }catch (e){
+            setMessage(""+e)
+        }
+
+    })
     return(
-        <main className={"flex flex-col mt-4 gap-7 w-[80%] justify-center items-center m-[0 auto]"}>
+        <main className={` flex flex-col mt-4 gap-7 w-[85%] justify-center items-center m-[0 auto]`}>
             <div>{message}</div>
-            <div className={"flex flex-col gap-2"}>
+            <div className={`flex flex-col gap-2 ${showChangeProfileImageForm&&"blur-2xl"}`}>
                 <p className={"font-bold mb-2"}>Profile picture</p>
                 <div className={"flex gap-4 items-center"}>
                     <Image
-                        src={"https://i.pinimg.com/236x/be/38/78/be3878c34f93d1663a6e5f6af4b78e9c.jpg"}
+                        src={userProfileImage}
                         alt={"profile photo"}
                         height={100}
                         width={100}
@@ -62,14 +93,59 @@ export default function ProfilePage(){
                     ></Image>
                     <div className={"flex gap-3 [&>*]:rounded-xl [&>*]:cursor-pointer" +
                         " [&>*]:w-fit [&>*]:h-fit [&>*]:p-4 [&>*]:font-bold "}>
-                        <button className={"bg-blue-500"}>Change Picture</button>
+                        <button className={"bg-blue-500"} onClick={()=>setShowProfileImageForm(!showChangeProfileImageForm)}>Change Picture</button>
                         <button className={"bg-red-500"}>DeletePicture</button>
                     </div>
 
                 </div>
             </div>
+            {showChangeProfileImageForm&&
+                <div className={"absolute z-999 bg-gray-600 " +
+                    " shadow-xl w-[20%] " +
+                    "  border border-gray-600  h-50 p-2 w-72 rounded-xl"}>
+                    {message &&<div>{message}</div>}
+                    <form className={"flex items-center flex-col p-3"}>
+                        <label>Profile image:</label>
+                        <div className={"p-2 gap-2 flex flex-col"}>
+                            <input type={"file"}
+                                   placeholder={"select image..."}
+                                   className={"border ] border-gray-700 rounded-xl p-2 cursor-pointer"}
+                                   onChange={(e)=> {
+                                       if(e.target.files !==null) {
+                                           setUploadedProfileImage(e.target.files[0])
+                                       }else {
+                                           setMessage("Profile image should not be blank")
+                                       }
+                                   }}
+                            />
+                            <div className={"flex gap-2 justify-center mt-2 [&>*]:font-bold"}>
+                                <button
+                                    className={"bg-blue-500 cursor-pointer p-2 rounded-xl"}
+                                    onClick={(e)=> {
+                                        e.preventDefault()
+                                        setShowProfileImageForm(false)
+                                        uploadProfilePhoto()
+                                    }}
+                                    type={"button"}>
+                                    Submit
+                                </button>
+                                <button
+                                    className={"bg-red-500 cursor-pointer p-2 rounded-xl"}
+                                    onClick={()=>setShowProfileImageForm(false)}
+                                    type={"button"}>
+                                    cancel
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>}
             {/*github,email,linkedin,mobile,username,description*/}
-            <form className={"flex gap-2 [&>input]:text-gray-400 [&>input]:outline-0 flex-col [&>label]:font-bold [&>input]:p-2 [&>input]:border" +
+            <form className={"flex gap-2 " +
+                `${showChangeProfileImageForm&&"blur-2xl"}`
+                +" [&>input]:text-gray-400 [&>input]:outline-0 flex-col" +
+                " [&>label]:font-bold [&>input]:p-2 [&>input]:border" +
                 " [&>input]:border-gray-600 [&>input]:rounded-xl "}>
                 <label>Profile name</label>
                 <input
