a45de77cab4ab2bb28ed149e455d2b6cd72b61b4
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-26 19:07:42 +0300

    manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-        <title></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title></title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="manifest" href="/manifest.json"/>
     </head>
-      <body
+    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
