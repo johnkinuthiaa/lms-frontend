a0760ab315e549912441664e7c509bb8c551f7ad
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-28 00:26:14 +0300

    mobile menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,25 +1,51 @@
+"use client"
 import {useRouter} from "next/navigation";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {useState} from "react"
 
 export default function Header(){
     const router =useRouter()
+    const[openMobileMenu,setOpenMobileMenu] =useState<boolean>(false)
     return(
-        <header className={"flex mt-4 mr-2 justify-between items-center mb-10"}>
-            <div className={"font-bold text-xl p-1"}>Nevani Learning</div>
-            <div className={"flex gap-2 [&>*]:pt-2 [&>*]:pb-2 [&>*]:pr-4 [&>*]:pl-4 [&>*]:rounded [&>*]:cursor-pointer"}>
-                <button className={"bg-blue-600"}
+        <div>
+            <header className={`flex mt-4 mr-2 justify-between items-center mb-10`}>
+                <div className={"font-bold text-xl p-1"}>Nevani Learning</div>
+                <div className={"flex gap-4 align-center"}>
+                    <div className={"flex gap-2 [&>*]:pt-2 [&>*]:pb-2 [&>*]:pr-4 [&>*]:pl-4 [&>*]:rounded [&>*]:cursor-pointer"}>
+                        <button className={"bg-blue-600"}
+                                onClick={(e)=>{
+                                    e.preventDefault()
+                                    router.push("/login")
+                                }}
+                        >Login</button>
+                        <button
+                            className={"bg-white text-black"}
+                            onClick={(e)=>{
+                                e.preventDefault()
+                                router.push("/register")
+                            }}
+                        >Sign up</button>
+                    </div>
+                    <button
+                        className={"cursor-pointer"}
                         onClick={(e)=>{
                             e.preventDefault()
-                            router.push("/login")
+                            setOpenMobileMenu(!openMobileMenu)
                         }}
-                >Login</button>
-                <button
-                    className={"bg-white text-black"}
-                    onClick={(e)=>{
-                        e.preventDefault()
-                        router.push("/register")
-                    }}
-                >Sign up</button>
-            </div>
-        </header>
+
+                    ><MoreVertIcon/></button>
+                </div>
+            </header>
+            {openMobileMenu &&
+                <aside className={"absolute -mt-4 bg-black w-[55%] border-r border-r-gray-700 h-full z-999 "}>
+                    <nav className={"flex ml-4"}>
+                        <ul className={"p-2 flex flex-col [&>li]:mb-4 [&>li]:cursor-pointer"}>
+                            <li className={""}>Home</li>
+                            <li>Contact</li>
+                            <li>About us</li>
+                        </ul>
+                    </nav>
+                </aside>}
+        </div>
     )
 }
