61ddc6d058e3f51dffaae07e980d783ca3eb36c3
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-28 00:38:56 +0300

    hide mobile menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,6 +2,7 @@
 import {useRouter} from "next/navigation";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {useState} from "react"
+import {Home, Info, Phone} from "@mui/icons-material";
 
 export default function Header(){
     const router =useRouter()
@@ -10,8 +11,17 @@ export default function Header(){
         <div>
             <header className={`flex mt-4 mr-2 justify-between items-center mb-10`}>
                 <div className={"font-bold text-xl p-1"}>Nevani Learning</div>
+                <div className={"header__nav"}>
+                    <nav className={"flex ml-4"}>
+                        <ul className={"p-2 flex gap-3 [&>li]:mb-4 [&>li]:gap-2 [&>li]:font-bold [&>li]:cursor-pointer"}>
+                            <li onClick={()=>router.push("/")}>Home</li>
+                            <li>Contact</li>
+                            <li>About us</li>
+                        </ul>
+                    </nav>
+                </div>
                 <div className={"flex gap-4 align-center"}>
-                    <div className={"flex gap-2 [&>*]:pt-2 [&>*]:pb-2 [&>*]:pr-4 [&>*]:pl-4 [&>*]:rounded [&>*]:cursor-pointer"}>
+                    <div className={"flex gap-2 [&>*]:pt-2 [&>*]:pb-2 [&>*]:pr-3 [&>*]:pl-3 [&>*]:rounded [&>*]:cursor-pointer"}>
                         <button className={"bg-blue-600"}
                                 onClick={(e)=>{
                                     e.preventDefault()
@@ -27,7 +37,7 @@ export default function Header(){
                         >Sign up</button>
                     </div>
                     <button
-                        className={"cursor-pointer"}
+                        className={"cursor-pointer lg:hidden md:hidden"}
                         onClick={(e)=>{
                             e.preventDefault()
                             setOpenMobileMenu(!openMobileMenu)
@@ -37,12 +47,12 @@ export default function Header(){
                 </div>
             </header>
             {openMobileMenu &&
-                <aside className={"absolute -mt-4 bg-black w-[55%] border-r border-r-gray-700 h-full z-999 "}>
+                <aside className={"absolute -mt-4 lg:hidden md:hidden bg-black w-[55%] border-r border-r-gray-700 h-full z-999 "}>
                     <nav className={"flex ml-4"}>
-                        <ul className={"p-2 flex flex-col [&>li]:mb-4 [&>li]:cursor-pointer"}>
-                            <li className={""}>Home</li>
-                            <li>Contact</li>
-                            <li>About us</li>
+                        <ul className={"p-2 flex flex-col [&>li]:mb-4 [&>li]:flex [&>li]:gap-2 [&>li]:items-center [&>li]:cursor-pointer"}>
+                            <li onClick={()=>router.push("/")}><Home/>Home</li>
+                            <li><Phone/>Contact</li>
+                            <li><Info/>About us</li>
                         </ul>
                     </nav>
                 </aside>}
